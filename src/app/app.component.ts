09b9990607878a2efb2fa5f6c10183495a60c3fd
Arnaud-Bdr <arn.boudier@gmail.com>
2022-01-03 21:31:49 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css', '../styles.css' ]
 })
 export class AppComponent  {
   name = 'Chess Pelo';
