f7fd290a80716073aa4ba96bd8bc85e9b2629098
Arnaud-Bdr <arn.boudier@gmail.com>
2022-01-08 16:06:22 +0100

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,10 @@ import { AppComponent } from './app.component';
 import { ChessboardComponent } from './chessboard/chessboard.component';
 import { ChesscellComponent } from './chesscell/chesscell.component';
 import { GameService } from './services/game.service';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, DragDropModule],
   declarations: [AppComponent, ChessboardComponent, ChesscellComponent],
   bootstrap: [AppComponent],
   providers: [GameService],
